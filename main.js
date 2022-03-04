41febe8f5f2fdd94678cd4be1055e28b9d56f913
Sydnei D <99233457+sydneicodes@users.noreply.github.com>
2022-03-04 00:02:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 function toggleDarkMode() {
     let bodyElement = document.getElementById("main-background"); 
     let darkModeButtonElement = document.getElementById("light-mode-btn");
+    let yogaSections = document.getElementsByClassName("yoga-section");
+    for (let index = 0; index < yogaSections.length; index++) {
+        if (yogaSections[index].classList.contains("light")) {
+            yogaSections[index].classList.remove("light");
+            yogaSections[index].classList.add("dark");
+        }  
 
-     // if it's in dark mode, change it to light mode 
-     if (bodyElement.style.background === "white") {
-        bodyElement.style.background = "#FBBE81";
-        darkModeButtonElement.innerHTML = "Light Mode";
-    }
-    // if background is NOT orange, change background to white
-    else {
-        bodyElement.style.background = "white"; //set background to white
-        darkModeButtonElement.innerHTML = "Dark Mode";
+        else if (yogaSections[index].classList.contains("dark")){
+            yogaSections[index].classList.remove("dark");
+            yogaSections[index].classList.add("light");
+        }
     }
 }
+
